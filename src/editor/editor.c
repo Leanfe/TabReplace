df4885913edbda37193b4215a031d91470f7755e
Leanfe <l.kapitman@gmail.com>
2022-10-30 15:00:27 +0200

    Fix this shit

--- src/editor/editor.c
+++ src/editor/editor.c
@@ -10,8 +10,8 @@
 char* deTab(const char* input_string, const int tab_replaces) {
     int len = strlen(input_string);
     char* output_string = (char*) malloc((tab_replaces*len+1)*sizeof(char));
-
     int j = 0;
+
     for (int i = 0; i < len; i++) {
         if (input_string[i] == '\t') {
             //Заполняем массив нужными символами.
@@ -20,8 +20,11 @@ char* deTab(const char* input_string, const int tab_replaces) {
              * PARAM 2 – код символа для заполнения.
              * PARAM 3 – размер заполняемой части массива в байтах.
              */
-            memset(output_string+j, ' ', tab_replaces*sizeof(char));
-            j += tab_replaces;
+            int resultTabs = tab_replaces-(i%tab_replaces);
+            for (int k = 0; k < resultTabs; k++) {
+                output_string[j] = ' ';
+                j++;
+            }
         } else {
             output_string[j] = input_string[i];
             j++;
@@ -46,4 +49,5 @@ void replaceWithPush(FILE* in, FILE* output, int tab_replaces) {
     while (fgets(buffer, BUFSIZ, in)) {
         fputs(deTab(buffer, tab_replaces), output);
     }
+
 }
